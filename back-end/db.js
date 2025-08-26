01da881c5e685d3edb8fe2318cab93197b1d7301
NAVEEN KUMAR S <naveenkumartceit@gmail.com>
2025-08-26 21:42:43 +0530

    Update db.js

--- back-end/db.js
+++ back-end/db.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://nknk:blood@ticket.s3d29.mongodb.net/?retryWrites=true&w=majority&appName=ticket', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("✅ Connected to MongoDB"))
+.catch(err => console.error("❌ MongoDB connection error:", err));
+
 
 const db = mongoose.connection;
 
