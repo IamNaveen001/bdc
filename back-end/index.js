c09809a7907d5f99b13771791502d30687cfe3e2
NAVEEN KUMAR S <naveenkumartceit@gmail.com>
2025-08-26 21:38:50 +0530

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -5,13 +5,12 @@ const db = require('./db');
 const donorRoutes = require('./routes/donorRoutes');
 
 const app = express();
-const port = 3000;
+
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api/donors', donorRoutes);
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
